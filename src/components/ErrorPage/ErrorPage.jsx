d9888bbafd7e41f60de853894f1f94d8a8f9373c
rejaul360 <rejaraju2k22@gmail.com>
2023-04-12 03:04:58 +0600

    edit error page

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import './ErrorPage.css'
 
 const ErrorPage = () => {
     return (
-        <div>
-            <h1 className='text-center text-4xl mt-24'>404 not found</h1>
+        <div className='not-found'>
+            <h5>OPPS !.... SORRY</h5>
+            <h2 > Page Not Found</h2>
+            <h1 >404 </h1>
+      <p>We are sorry, but the page you requested could not be found. Try again the right path.</p>
         </div>
     );
 };
